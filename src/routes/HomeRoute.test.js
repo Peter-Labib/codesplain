207910fa29ad3237b26bde2e74d740899c5553a7
Peter Labib <peter.adel.labib@gmail.com>
2023-03-29 00:00:03 +0200

    making a reusable create server function

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,30 +1,27 @@
 import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
+import { createServer } from '../test/server';
 
 import HomeRoute from './HomeRoute';
 
-const handler = [
-  rest.get('/api/repositories', (req, res, ctx) => {
-    const language = req.url.searchParams.get('q').split('language:')[1];
+const handlers = [
+  {
+    path: '/api/repositories',
+    method: 'get',
+    res: (req) => {
+      const language = req.url.searchParams.get('q').split('language:')[1];
 
-    return res(
-      ctx.json({
+      return {
         items: [
           { id: '1', full_name: `first ${language} repo` },
           { id: '2', full_name: `second ${language} repo` },
         ],
-      })
-    );
-  }),
+      };
+    },
+  },
 ];
 
-const server = setupServer(...handler);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+createServer(handlers);
 
 test('render head and two links for each lang', async () => {
   const languages = ['javascript', 'typescript', 'rust', 'go'];
---
+++ src/test/server.js
@@ -0,0 +1,16 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+export const createServer = (configHandler) => {
+  const handlers = configHandler.map((config) => {
+    return rest[config.method](config.path, (req, res, ctx) => {
+      return res(ctx.json(config.res(req, res, ctx)));
+    });
+  });
+
+  const server = setupServer(...handlers);
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+};
